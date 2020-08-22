2a40e11ee376c1d633ffafa5f74c775077da1db9
takurinton <takuirnton@gmail.com>
2020-08-22 10:36:51 +0900

    add api

---
+++ retro/src/api/get.js
@@ -0,0 +1,17 @@
+const toJson = async (res) => {
+    const js = await res.json()
+    if (res.ok) {
+        return js
+    } else {
+        throw new Error(js.message)
+    }
+}
+
+
+export const getMainAPI = async () => {
+    const resp = await fetch(`https://your-domain.com`, {
+        method: "GET",
+        credentials: "same-origin",
+    })
+    return await toJson(resp)
+}
